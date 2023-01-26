a3fe9e31a9d343127d3b0b4d01b3f43e3a2b836d
yannapadillo <lzpadillo@student.apc.edu.ph>
2023-01-26 23:27:07 +0800

    Lesson 22.4 - DOM Elements

--- week5/script.js
+++ week5/script.js
@@ -1,3 +1,26 @@
 //Lesson 22.3 DOM Document
 let list = document.querySelectorAll('.list');
 		let itemBox = document.querySelectorAll('.itemBox');
+//Lesson 22.4 - DOM Elements
+		for(let i = 0; i < list.length; i++){
+		    list[i].addEventListener('click', function(){
+		        for(let j = 0; j< list.length; j++){
+		            list[j].classList.remove('active');
+		        }
+
+		        this.classList.add('active');
+		        
+
+		        let dataFilter = this.getAttribute('data-filter');
+
+		        for(let k = 0; k < itemBox.length; k++){
+		            itemBox[k].classList.remove('active');
+		            itemBox[k].classList.add('hide');
+
+		            if(itemBox[k].getAttribute('data-item') == dataFilter || dataFilter == "all"){
+		                itemBox[k].classList.remove('hide');
+		                itemBox[k].classList.add('active');
+		            }
+		        }
+		    });
+		}
