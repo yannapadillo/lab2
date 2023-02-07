07e1870540396c518cce1c481e3096aeb38d5257
yannapadillo <lzpadillo@student.apc.edu.ph>
2023-02-07 14:07:52 +0800

    updated validation complete

--- week9/validation_complete.php
+++ week9/validation_complete.php
@@ -1,12 +1,15 @@
-<!DOCTYPE HTML>  
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
 .error {color: #FF0000;}
 </style>
 </head>
-<body>  
-
+<body>
 <?php
 // define variables and set to empty values
 $nameErr = $emailErr = $genderErr = $websiteErr = "";
@@ -100,38 +103,30 @@ echo "<br>";
 echo $gender;
 ?>
 
-
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") 
-{
-
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "webprogss211db";
-	
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-	}
-	
-	$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-	VALUES ('$name', ' ', '$email')";
-	
-	if ($conn->query($sql) === TRUE) {
-	echo "New record created successfully";
-	} else {
-	echo "Error: " . $sql . "<br>" . $conn->error;
-	}
-	
-	$conn->close();
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('$name', '', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
-
-
 
 
 
