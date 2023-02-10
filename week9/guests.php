a1671804abead86a617ff0c21e577aa914f66785
yannapadillo <92007528+yannapadillo@users.noreply.github.com>
2023-02-10 09:48:53 +0800

    Add files via upload

--- week9/guests.php
+++ week9/guests.php
@@ -1,26 +1,49 @@
 <?php
-$servername = "localhost";
-$username = "webprogss211";
-$password = "webprogss211";
-$dbname = "webprogss211";
+        $servername = "localhost";
+        $username = "webprogss211";
+        $password = "webprogss211";
+        $dbname = "webprogss211";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
 
-$sql = "SELECT id, Name, Email FROM lzpadillo_myguests";
-$result = $conn->query($sql);
+        $sql = "SELECT * FROM lzpadillo_myguests";
+        $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["Name"]. " - Email " . $row["Email"]. "<br>";
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
+        if ($result->num_rows > 0) {
+            echo "<table class='table'>
+            <thead>
+                <tr>
+                <th>ID</th>
+                    <th>Name</th>
+                    <th>Gender</th>
+                    <th>Comments</th>
+                    <th>Email</th>
+                    <th>Website</th>
+                    <th>Registration Date</th>
+                    </tr>
+                    </thead>";
+                    // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tbody>
+                    <tr>
+                        <td>".$row["id"]."</td>
+                        <td>".$row["name"]."</td>
+                        <td>".$row["gender"]."</td>
+                        <td>".$row["comments"]."</td>
+                        <td>".$row["email"]."</td>
+                        <td>".$row["website"]."</td>
+                        <td>".$row["reg_date"]."</td>
+                        </tr>
+                        </tbody>";
+                    }
+        echo "</table>";
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
 ?>
