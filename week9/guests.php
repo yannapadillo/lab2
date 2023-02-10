513945cdd8fba49909f21b6f83ef3d8cff980d6c
yannapadillo <lzpadillo@student.apc.edu.ph>
2023-02-10 12:11:43 +0800

    updated the guests.php

--- week9/guests.php
+++ week9/guests.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, Name, Email FROM mapinlac_myguests";
+$sql = "SELECT id, Name, Email FROM lzpadillo_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
