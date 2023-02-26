c1cc8fbdf2e0c3d61c4b2083aa7196ccd07f3d2c
yannapadillo <lzpadillo@student.apc.edu.ph>
2023-02-26 22:14:38 +0800

    adding the profile page and news section

--- week9/index.php
+++ week9/index.php
@@ -388,121 +388,94 @@
   <footer class="footer section" id="contact">
     <h2 class="section-title">Get in Touch</h2>
     <?php
-// define variables and set to empty values
-$nameErr = $emailErr = $genderErr = $websiteErr = "";
-$name = $email = $gender = $comment = $website = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed";
+namespace App\Controllers;
+
+use App\Models\GuestsModel;
+use CodeIgniter\Exceptions\PageNotFoundException;
+
+class Guests extends BaseController
+{
+  public function index()
+  {
+    $model = model(GuestsModel::class);
+
+    $data = [
+      'guests'  => $model->getGuests(),
+      'title' => 'Guest list',
+    ];
+
+    return view('templates/header', $data)
+      . view('guests/index')
+      . view('templates/footer');
+  }
+
+  public function view($id = null)
+  {
+    $model = model(GuestsModel::class);
+
+    $data['guests'] = $model->getGuests($id);
+
+    if (empty($data['guests'])) {
+      throw new PageNotFoundException('Cannot find the guest with the ID: ' . $id);
     }
+
+    $data['title'] = 'Guest list';
+
+    return view('templates/header', $data)
+      . view('guests/view')
+      . view('templates/footer');
   }
-  
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format";
+
+  public function create()
+  {
+    helper('form');
+
+    // Checks whether the form is submitted.
+    if (!$this->request->is('post')) {
+      // The form is not submitted, so returns the form.
+      return view('templates/header', ['title' => 'Add a guest'])
+        . view('guests/create')
+        . view('templates/footer');
     }
-  }
-    
-  if (empty($_POST["website"])) {
-    $website = "";
-  } else {
-    $website = test_input($_POST["website"]);
-    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-      $websiteErr = "Invalid URL";
+
+    $post = $this->request->getPost([
+      'name',
+      'email',
+      'website',
+      'comment',
+      'gender',
+    ]);
+
+    // Checks whether the submitted data passed the validation rules.
+    if (!$this->validateData($post, [
+      'name' => 'required|max_length[70]|min_length[3]',
+      'email' => 'required|valid_email|max_length[50]|min_length[6]',
+      'website' => 'valid_url_strict|max_length[100]|min_length[4]',
+      'comment'  => 'max_length[5000]|min_length[10]',
+      'gender' => 'max_length[6]|min_length[4]',
+    ])) {
+      // The validation fails, so returns the form.
+      return view('templates/header', ['title' => 'Add a guest'])
+        . view('guests/create')
+        . view('templates/footer');
     }
+
+    $model = model(GuestsModel::class);
+
+    $model->save([
+      'name' => $post['name'],
+      'email' => $post['email'],
+      'website' => $post['website'],
+      'comment' => $post['comment'],
+      'gender'  => $post['gender'],
+    ]);
+
+    return view('templates/header', ['title' => 'Add a guest'])
+      . view('guests/success')
+      . view('templates/footer');
   }
-
-  if (empty($_POST["comment"])) {
-    $comment = "";
-  } else {
-    $comment = test_input($_POST["comment"]);
-  }
-
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  }
 }
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?>
-<p><span class="error">* required field</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  Name: <input type="text" name="name" value="<?php echo $name;?>">
-  <span class="error">* <?php echo $nameErr;?></span>
-  <br><br>
-  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-  <span class="error">* <?php echo $emailErr;?></span>
-  <br><br>
-  Website: <input type="text" name="website" value="<?php echo $website;?>">
-  <span class="error"><?php echo $websiteErr;?></span>
-  <br><br>
-  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-  <br><br>
-  Gender:
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
-  <span class="error">* <?php echo $genderErr;?></span>
-  <br><br>
-  <input type="submit" name="submit" value="Submit">  
-</form>
-
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
-?>
-
-
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "webprogss211db";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('$firstname', '$lastname', '$email')";
-
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-?>
 <br><br>
     <div class="footer-social">
       <a href="https://www.linkedin.com/in/lyanna-padillo-8ba1b321a/" target="_blank" class="footer-icon"><i
